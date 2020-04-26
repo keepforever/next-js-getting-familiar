95c8a50aacf60be9b53e10936ca938584fc2505a
keepforever <briancilenti@gmail.com>
2020-04-26 18:31:55 -0400

    added boiler sass

--- comps/SiteLayout.jsx
+++ comps/SiteLayout.jsx
@@ -1,10 +1,14 @@
 import Link from 'next/link';
 import TopNavBar from '../comps/TopNavBar';
+import '../scss/styles.scss';
 
 const SiteLayout = ({ children }) => (
     <div>
         <TopNavBar />
         <h1>Hello Site Layout</h1>
+        <div className="row">
+            <div className="btn btn-primary">Test Button</div>
+        </div>
         <div>{children}</div>
     </div>
 );
